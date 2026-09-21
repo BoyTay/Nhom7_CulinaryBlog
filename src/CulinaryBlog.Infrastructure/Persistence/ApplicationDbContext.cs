fa4873e8dda1a0bbc689ac2c0a4d567e1b4f65fe
Girrint <truongphan172@gmail.com>
2026-09-22 00:02:51 +0700

    feat: src: infrastructure: register recipe db sets

--- src/CulinaryBlog.Infrastructure/Persistence/ApplicationDbContext.cs
+++ src/CulinaryBlog.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 using CulinaryBlog.Application.Abstractions.Persistence;
+using CulinaryBlog.Domain.Recipes;
 using Microsoft.EntityFrameworkCore;
 
 namespace CulinaryBlog.Infrastructure.Persistence;
@@ -6,6 +7,14 @@ namespace CulinaryBlog.Infrastructure.Persistence;
 public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
     : DbContext(options), IDataSession
 {
+    public DbSet<Recipe> Recipes => Set<Recipe>();
+
+    public DbSet<RecipeStep> RecipeSteps => Set<RecipeStep>();
+
+    public DbSet<RecipeIngredient> RecipeIngredients => Set<RecipeIngredient>();
+
+    public DbSet<RecipeImage> RecipeImages => Set<RecipeImage>();
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
